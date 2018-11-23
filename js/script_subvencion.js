1c893f593ab70b14b5331463f198bf76db14a937
johnny <ceaceajohnny@gmail.com>
2018-11-23 03:46:53 -0300

    mantenedor subvencion
    mostrar, ingresar, eliminar

--- js/script_subvencion.js
+++ js/script_subvencion.js
@@ -1,30 +1,30 @@
-listarCuenta();
+listarSubvencion();
 
-function listarCuenta(){
+function listarSubvencion(){
 
 
 		$.ajax({
-			url:"./metodos_ajax/cuentas/mostrar_cuenta.php",
+			url:"./metodos_ajax/subvencion/mostrar_subvencion.php",
 			method:"POST",
 			success:function(respuesta){
-				 $("#contenedor_listado_cuenta").html(respuesta);
+				 $("#contenedor_listado_subvencion").html(respuesta);
 			}
 		});
 }
 
-function guardarCuenta(){
+function guardarSubvencion(){
 
 			$.ajax({
-				url:"./metodos_ajax/cuentas/crear_cuenta.php",
+				url:"./metodos_ajax/subvencion/crear_subvencion.php",
 				method:"POST",
-				data: $("#formulario_modal_cuenta").serialize(),
+				data: $("#formulario_modal_subvencion").serialize(),
 				success:function(respuesta){
 					  // alert(respuesta);
 
 					 if(respuesta==1){
 						 swal("Guardado","Los datos se han guardado correctamente.","success");
-						 $("#modal_cuenta").modal('hide');
-						 listarCuenta();
+						 $("#modal_subvencion").modal('hide');
+						 listarSubvencion();
 					 }else if(respuesta==2){
 						 swal("Ocurrió un error","Recargue la página e intente nuevamente.","error");
 					 }
@@ -46,36 +46,36 @@ function guardarCuenta(){
 
 function cargarDatosModificar(id){
 
-  var numero_cuenta = $("#txt_numero_"+id).html();
-  var nombre_cuenta = $("#txt_nombre_"+id).html();
+  var txt_id_subvencion = $("#txt_id_subvencion_"+id).html();
+  var txt_subvencion = $("#txt_subvencion_"+id).html();
 
 
 	//carga la informacion recibida en el modal
-	$('#txt_numero_cuenta').val(numero_cuenta);
-	$('#txt_nombre_cuenta').val(nombre_cuenta);
+	$('#txt_id_subvencion').val(txt_id_subvencion);
+	$('#txt_subvencion').val(txt_subvencion);
 
   //pone el campo como no editable
-	$('#txt_numero_cuenta').attr("readonly",true);
+	$('#txt_id_subvencion').attr("readonly",true);
 
   //cambia la funcion que se ejecuta al enviar el formulario
-	$("#formulario_modal_cuenta").attr("action","javascript:modificarCuenta()");
+	$("#formulario_modal_subvencion").attr("action","javascript:modificarSubvencion()");
 
 }
 
 
-function modificarCuenta(){
+function modificarSubvencion(){
 
 			$.ajax({
-				url:"./metodos_ajax/cuentas/modificar_cuenta.php",
+				url:"./metodos_ajax/subvencion/modificar_subvencion.php",
 				method:"POST",
-				data: $("#formulario_modal_cuenta").serialize(),
+				data: $("#formulario_modal_subvencion").serialize(),
 				success:function(respuesta){
-					 // alert(respuesta);
+					 alert(respuesta);
 
 					 if(respuesta==1){
 						 swal("Guardado","Los datos se han guardado correctamente.","success");
-						 $("#modal_cuenta").modal('hide');
-						 listarCuenta();
+						 $("#modal_subvencion").modal('hide');
+						 listarSubvencion();
 					 }else if(respuesta==2){
 						 swal("Ocurrió un error","Recargue la página e intente nuevamente.","error");
 				   }
@@ -84,16 +84,16 @@ function modificarCuenta(){
 
 }
 
-function eliminarCuenta(id){
+function eliminarSubvencion(id){
 
 			$.ajax({
-				url:"./metodos_ajax/cuentas/eliminar_cuenta.php?id="+id,
+				url:"./metodos_ajax/subvencion/eliminar_subvencion.php?id="+id,
 				method:"POST",
 				success:function(respuesta){
-					 // alert(respuesta);
+					 alert(respuesta);
 					 if(respuesta==1){
 						 swal("Eliminado correctamente","Los datos se han guardado correctamente.","success");
-						 listarCuenta();
+						 listarSubvencion();
 					 }else if(respuesta==2){
 						 swal("Ocurrió un error","Recargue la página e intente nuevamente.","error");
 					 }
