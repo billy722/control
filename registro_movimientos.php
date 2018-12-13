d766643d201d219d76eb68724db686f4577536f8
johnny <ceaceajohnny@gmail.com>
2018-12-13 11:28:25 -0300

    cambio pag pricipal
    besos de amor

--- registro_movimientos.php
+++ registro_movimientos.php
@@ -7,7 +7,7 @@ require_once './clases/TipoMovimiento.php';
 require_once './clases/Subvencion.php';
 require_once './clases/Colegio.php';
 require_once './clases/Cuenta.php';
-require_once './clases/Estado.php';
+require_once './clases/Estado_movimiento.php';
 
 comprobarSession();
 $usuario= new Usuario();
@@ -75,7 +75,7 @@ $usuario= $usuario->obtenerUsuarioActual();
                   <center>OPCIONES</center>
               </div>
               <div class="card-body">
-                   <button onclick="limpiarModal()" data-toggle="modal" data-target="#modal_movimientos" class="active btn btn-info col-12">Agregar</button>
+                   <button class="active btn btn-info col-12">Ver lista</button>
                      <!-- <hr> -->
                    <!-- <button  class="active btn btn-info col-12">Seguimiento</button> -->
               </div>
@@ -83,7 +83,7 @@ $usuario= $usuario->obtenerUsuarioActual();
 
         </div>
 
-         <div class="col-12 col-md-10">
+         <!-- <div class="col-12 col-md-10">
 
              <div id='' style="" class=" card col-12 ">
 
@@ -94,28 +94,28 @@ $usuario= $usuario->obtenerUsuarioActual();
 
              </div>
 
-         </div>
+         </div> -->
     </div>
 
     <div class="container-fluid">
       <hr>
     </div>
 
-       <div class="col-12 col-md-12">
+    <!--   <div class="col-12 col-md-12">
 
            <div id='' style="" class="  col-12 ">
 
                <div class="card-header  text-info">Registro de movimientos</div>
                <div class="card-body">
-                 <!-- <h5 class="card-title">Titulo</h5>
+                  <h5 class="card-title">Titulo</h5>
                  <p class="card-text">texto prueba</p> -->
-                 <div class="row" id="contenedor_registro_movimientos">
+                 <!-- <div class="row" id="contenedor_registro_movimientos">
 
                  </div>
 
                </div>
 
-           </div>
+           </div> -->
 
        </div>
 
@@ -125,15 +125,10 @@ $usuario= $usuario->obtenerUsuarioActual();
 
 
 <!-- MODAL INGRESAR CORRESPONDENCIA-->
-<div class="modal fade" id="modal_movimientos" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog modal-lg" role="document">
-  <div class="modal-content">
-
-    <div class="modal-header bg-info">
-      <h5 class="modal-title text-white"  id="myModalLabel">Ingreso Movimiento</h5>
-      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    </div>
-    <div class="modal-body">
+<div class="container">
+<div class="card">
+  <h5 class="card-header">Nuevo Movimientos</h5>
+  <div class="card-body">
 
       <form id="formulario_modal_movimientos" class="" action="javascript:registrarModificarMovimiento()" method="post">
 
@@ -313,7 +308,7 @@ $usuario= $usuario->obtenerUsuarioActual();
                         <select class="form-control" name="select_estado" id="select_estado">
                           <option value="" selected disabled>Seleccione:</option>
                             <?php
-                                $Estado = new Estado();
+                                $Estado = new Estado_movimiento();
                                 $Estado->setTabla("tb_estado_movimiento");
                                 $listaEstados = $Estado->obtenerEstados();
 
@@ -326,25 +321,21 @@ $usuario= $usuario->obtenerUsuarioActual();
 
                   </div>
 
+
             </div>
 
 
-              <div class="form-group" >
                 <div class="col-12">
                   <button class="btn btn-info btn-block" type="submit" name="button">Guardar</button>
                 </div>
-              </div>
-
-
-      </form>
-
-    </div>
-
-
-  </div>
-  </div>
+            </form>
+</div>
+</div>
 </div>
 
+<div class="container">
+  <br>
+</div>
 
 <!-- MODAL REVISAR CORRESPONDENCIA-->
 <div class="modal fade" id="modal_revisar_correspondencia" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -464,3 +455,225 @@ $usuario= $usuario->obtenerUsuarioActual();
 
 </body>
 </html>
+
+
+<!-- MODAL INGRESAR CORRESPONDENCIA-->
+<!-- <div class="modal fade" id="modal_movimientos" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-content">
+
+    <div class="modal-header bg-info">
+      <h5 class="modal-title text-white"  id="myModalLabel">Ingreso Movimiento</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    </div>
+    <div class="modal-body">
+
+      <form id="formulario_modal_movimientos" class="" action="javascript:registrarModificarMovimiento()" method="post">
+
+
+             <div class="form-group  border-info" >
+
+                  <input type="hidden" name="txt_id_movimiento" id="txt_id_movimiento">
+
+                  <div class="row">
+
+                      <div class="form-group col-4" >
+                             <label for="title" class="col-12 control-label">Fecha de ingreso</label>
+                             <input value="<?php //echo date('d-m-Y'); ?>" class="form-control" type="text" id="txt_fecha_ingreso" name="txt_fecha_ingreso" min="2018-01-01"  readonly placeholder="Dia/Mes/Año" >
+                      </div>
+
+                      <div class="form-group col-md-4" >
+
+                          <label for="title" class="col-12 control-label">Tipo movimiento</label>
+                          <select required class="form-control" name="select_tipo_movimiento" id="select_tipo_movimiento">
+                            <option value="" selected disabled>Seleccione:</option>
+                            <?php
+                                // $TipoMovimiento = new TipoMovimiento();
+                                // $listaTiposMovimientos = $TipoMovimiento->obtenerTiposMovimientos();
+                                //
+                                // while($filas = $listaTiposMovimientos->fetch_array()){
+                                //    echo '<option value="'.$filas['id_tipo_movimiento'].'">'.$filas['descripcion_tipo_movimiento'].'</option>';
+                                // }
+                             ?>
+                          </select>
+
+                      </div>
+
+                      <div class="form-group col-md-4" >
+
+                          <label for="title" class="col-12 control-label">Colegio</label>
+                          <select required class="form-control" name="select_colegio" id="select_colegio">
+                            <option value="" selected disabled>Seleccione:</option>
+                            <?php
+                                // $Colegio = new Colegio();
+                                // $listaColegios = $Colegio->obtenerColegios();
+                                //
+                                // while($filas = $listaColegios->fetch_array()){
+                                //     echo '<option value="'.$filas['rbd_colegio'].'">'.$filas['rbd_colegio'].': '.$filas['nombre_colegio'].'</option>';
+                                // }
+                              ?>
+                          </select>
+
+                      </div>
+
+                  </div>
+
+                  <div class="row">
+
+                      <div class="form-group col-md-4" >
+
+                          <label for="title" class="col-12 control-label">Subvencion</label>
+                          <select required onChange="cambiaSubvencion(this.value)" class="form-control" name="select_subvencion" id="select_subvencion">
+                            <option value="" selected disabled>Seleccione:</option>
+                            <?php
+                                // $Subvencion = new Subvencion();
+                                // $listaSubvenciones = $Subvencion->obtenerSubvencion();
+                                //
+                                // while($filas = $listaSubvenciones->fetch_array()){
+                                //     echo '<option value="'.$filas['id_subvencion'].'">'.$filas['subvencion'].'</option>';
+                                // }
+                              ?>
+                          </select>
+
+                      </div>
+
+                      <div class="form-group col-md-4" >
+                          <label for="title" class="col-12 control-label">Cuenta Presupuesto</label>
+                          <select required class="form-control" name="select_cuenta" id="select_cuenta">
+                            <option value="" selected disabled>Seleccione:</option>
+                            <?php
+                                // $Cuenta = new Cuenta();
+                                // $listaCuentas = $Cuenta->obtenerCuentas();
+                                //
+                                // while($filas = $listaCuentas->fetch_array()){
+                                //    echo '<option value="'.$filas['numero_cuenta'].'">'.$filas['numero_cuenta'].': '.$filas['nombre_cuenta'].'</option>';
+                                // }
+                             ?>
+                          </select>
+                      </div>
+
+                      <div class="form-group col-md-4" >
+                          <label for="title" class="col-12 control-label">Orden de compra</label>
+                          <input type="text" class="form-control" name="txt_orden_compra" placeholder="Orden de compra">
+                      </div>
+
+                  </div>
+
+                  <div class="row">
+
+                    <div class="form-group col-md-12" >
+                        <label for="title" class="col-12 control-label">Descripción</label>
+                        <textarea name="txt_descripcion" id="txt_descripcion" class="form-control " rows="2"></textarea>
+                    </div>
+
+                  </div>
+
+
+                  <!-- CONTENEDOR DE CAMPOS A MOSTRAR CUANDO LA SUBVENCION ES SEP -->
+                  <!-- <div id="contenedor_campos_sep" class="card bg-light mb-12 d-none" >
+                    <div class="card-header">
+                       <h5 class="card-title">Campos SEP</h5>
+                    </div>
+                    <div class="card-body bg-white">
+
+                       <div class="row" >
+                          <div class="form-group col-md-4" >
+                              <label for="title" class="col-12 control-label">Preferente</label>
+                              <input type="text" class="form-control" name="sep_preferente" placeholder="$">
+                          </div>
+                          <div class="form-group col-md-4" >
+                              <label for="title" class="col-12 control-label">Preferencia</label>
+                              <input type="text" class="form-control" name="sep_preferencia" placeholder="$">
+                          </div>
+                          <div class="form-group col-md-4" >
+                              <label for="title" class="col-12 control-label">Concentracion</label>
+                              <input type="text" class="form-control" name="sep_concentracion" placeholder="$">
+                          </div>
+                          <div class="form-group col-md-4" >
+                              <label for="title" class="col-12 control-label">Articulo 19</label>
+                              <input type="text" class="form-control" name="sep_articulo_19" placeholder="$">
+                          </div>
+                          <div class="form-group col-md-4" >
+                              <label for="title" class="col-12 control-label">Ajustes</label>
+                              <input type="text" class="form-control" name="sep_ajustes" placeholder="$">
+                          </div>
+                          <div class="form-group col-md-4" >
+                              <label for="title" class="col-12 control-label">Total</label>
+                              <input type="text" class="form-control" name="sep_total" placeholder="$">
+                          </div>
+                      </div>
+
+                    </div>
+                  </div> -->
+
+                  <!-- CONTENEDOR DE CAMPOS A MOSTRAR CUANDO LA SUBVENCION ES Sc-vtf -->
+                  <!-- <div id="contenedor_campos_Sc-vtf" class="card bg-light mb-12 d-none" >
+                    <div class="card-header">
+                       <h5 class="card-title">Campos Sc-vtf</h5>
+                    </div>
+                    <div class="card-body bg-white">
+
+                       <div class="row" >
+                          <div class="form-group col-md-6" >
+                              <label for="title" class="col-12 control-label">Subvencion Normal:</label>
+                              <input type="text" class="form-control" name="scvtf_normal" placeholder="$">
+                          </div>
+                          <div class="form-group col-md-6" >
+                              <label for="title" class="col-12 control-label">Subvencion de Nivelacion:</label>
+                              <input type="text" class="form-control" name="scvtf_nivelacion" placeholder="$">
+                          </div>
+
+                      </div>
+
+                    </div>
+                  </div>
+
+
+                  <div class="row">
+
+                    <div class="form-group col-md-4" >
+                        <label for="title" class="col-12 control-label">Monto</label>
+                        <input type="text" class="form-control-lg" name="txt_monto" placeholder="Ingrese Monto">
+                    </div>
+
+                  </div>
+
+
+                  <div class="row">
+
+                    <div id="contenedor_selector_estado" class="form-group col-md-12" >
+                        <label for="title" class="col-12 control-label">Estado</label>
+                        <select class="form-control" name="select_estado" id="select_estado">
+                          <option value="" selected disabled>Seleccione:</option>
+                            <?php
+                                // $Estado = new Estado();
+                                // $Estado->setTabla("tb_estado_movimiento");
+                                // $listaEstados = $Estado->obtenerEstados();
+                                //
+                                // while($filas = $listaEstados->fetch_array()){
+                                //    echo '<option value="'.$filas['id_estado'].'">'.$filas['descripcion_estado'].'</option>';
+                                // }
+                             ?>
+                        </select>
+                    </div>
+
+                  </div>
+
+            </div>
+
+
+              <div class="form-group" >
+                <div class="col-12">
+                  <button class="btn btn-info btn-block" type="submit" name="button">Guardar</button>
+                </div>
+              </div>
+
+
+      </form>
+
+    </div>
+
+
+  </div>
+  </div>
+</div> -->
